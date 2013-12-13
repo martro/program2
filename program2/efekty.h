c083b7080492c676d8f9122efc5a4a4c12878ea9
martro <marcintrojan@msn.com>
2013-12-14 00:58:46 +0100

    dodany poczatek funkcji lustro

--- program2/efekty.h
+++ program2/efekty.h
@@ -3,7 +3,20 @@
 
 element* odbicie_lustrzane poziom(element* first);
 {
+    element* temp=tymczas();
     wyswietl(lista);
+    printf("\nKtory plik odbic?\n");
+    while((getchar()) != '\n');
+    wybor=getchar();
+        temp=pozycja(wybor-1);
+    pamiec_strukt(temp);
+
+
+    for(i=0; i<temp->wys;i++)
+        free(temp->obraz[i]);
+    free(temp->obraz);
+    free(temp->nazwa_pliku);
+    free(temp);
 
 }
 
